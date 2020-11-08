a444f64ca3c8b9fac11c938353e943ce34eb1145
rutvikjha <56047961+rutvikjha@users.noreply.github.com>
2020-11-08 03:49:37 -0800

    Add files via upload

---
+++ covid_radar.ino
@@ -0,0 +1,101 @@
+// Includes the Servo library
+#include <Servo.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+/* Serial Output Guide:
+ *  In each line of serial output, angle (degrees) and distance (radians) is outputted in a comma seperated list i.e 147,20 or 138,201
+ *  At the end of a cycle of rotation temperature (celsius) and humidity (%) is outputted in a comma seperated list marked by 'T' i.e T20.44,65 or T22.45,68
+*/
+
+
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+#define DHTTYPE    DHT11     // DHT 11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 10;
+const int echoPin = 11;
+// Variables for the duration and the distance
+long duration;
+int distance;
+Servo myServo; // Creates a servo object for controlling the servo motor
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+  myServo.attach(12); // Defines on which pin is the servo motor attached
+  dht.begin();
+}
+
+
+void loop() {
+  // rotates the servo motor from 15 to 165 degrees
+  for(int i=15;i<=110;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  
+  Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print(distance); // Sends the distance value into the Serial Port
+  Serial.println();
+  }
+  // Repeats the previous lines from 165 to 15 degrees
+  for(int i=110;i>15;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.println();
+  }
+
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) { //if temperature reading fails return nothing
+    
+  }
+  else {
+    Serial.print("T");
+    Serial.print(event.temperature);
+  }
+
+  Serial.print(",");
+  
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) { //if humidity reading fails return nothing
+    
+  }
+  else {
+    Serial.print(event.relative_humidity);
+    Serial.println();
+  }
+}
+// Function for calculating the distance measured by the Ultrasonic sensor
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2;
+  return distance;
+}
